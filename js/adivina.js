2c18c6d45c758b6fc8e6848f7a309b055c80baf7
Alberto Abades Hoyas <alberto.abades@gmail.com>
2015-06-07 21:15:32 +0200

    Cambios de estilo en el js, sin comprobar funcionalidad completa

--- js/adivina.js
+++ js/adivina.js
@@ -14,65 +14,13 @@ var marker2;
 var currstate = 0;
 var state;
 
-	function goTo(State){
-
-		state= State - currstate;
-		if(state===0){
-			startGame();
-		}else{
-			history.go(state);
-		}
-	}
-
 $(document).ready(function(){ 
-	$("#selectCoords").val("");
-	$("#correctCoords").val("");
-	$("#name").val("");
-	$("#distance").val("");
-	$("#photos").val("");
-	$("#points").val("");
-	$("#juegoelegido").val("");
-	$("#nivel").val("");
 
 	map = L.map('mapa').setView([40.2838, -3.8215], 2);
 	// add an OpenStreetMap tile layer
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
-
-	var popup = L.popup();
-	var popup2 = L.popup();
-	function onMapClick(e) {
-		var marker = "";
-		var lat = e.latlng.toString();
-		coords = lat.substring(6);
-		marker1 = L.marker(e.latlng).addTo(map);
-		$("#selectCoords").val(coords);
-		dist=e.latlng.distanceTo(L.latLng(placecoords[0], placecoords[1]))/1000;
-		$("#distance").val(dist.toFixed(3));
-		$("#correctCoords").val("("+placecoords+")");
-	    $("#name").val(placetag);
-	    marker2 = L.marker(L.latLng(placecoords[1], placecoords[0])).addTo(map);
-	    clearTimeout(myVar);
-	    calcularPuntuacion();
-	    marcado = 1;
-	    saveHistory();
-	}
-
-	function startGame(){
-		if (marcado == 1){
-			map.removeLayer(marker1);
-			map.removeLayer(marker2);
-		}	
-		numPhotos = 0;
-		$.getJSON("juegos/"+juego, function(datos){
-			var place = datos.features[Math.floor(Math.random()*datos.features.length)];
-	        placecoords=place.geometry.coordinates;
-	        placetag=place.properties.Name;
-	        showPics(placetag);
-	        map.on('click', onMapClick);
-		});
-	}
 
     $("#startGame").click(function(){
     	if (name != " " && nivel != " "){
@@ -125,31 +73,6 @@ $(document).ready(function(){
     	$("#level").val("Dificil");
     });
 
-    function showPics(placetag){
-    	i=0;
-    	$.getJSON(flickerAPI,{
-			tags:placetag,
-			tagmode:"any",
-			format:"json"
-		}).done(function(data){
-			data = data.items.splice(0,30);
-			myVar = setInterval(function () {myTimer()}, 1000/nivel);
-			function myTimer() {
-				document.getElementById("fotos").innerHTML = "";
-				html = '<img src="' + data[i].media.m + '"width="500px" height="280px">';
-				document.getElementById("fotos").innerHTML = html;
-				i++;
-				numPhotos++;
-				$("#photos").val(numPhotos);
-			}
-		});
-    }
-
-    function calcularPuntuacion(){
-    	var puntuacion = dist*numPhotos;
-    	$("#points").val(puntuacion.toFixed(3));
-    }
-
     function saveHistory(){
 		var stateObj={
 			name:juego,
@@ -169,3 +92,62 @@ $(document).ready(function(){
 	});
 
 });
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function startGame(){
+	if (marcado == 1){
+		map.removeLayer(marker1);
+		map.removeLayer(marker2);
+	}	
+	numPhotos = 0;
+	$.getJSON("juegos/"+juego, function(datos){
+		var place = datos.features[Math.floor(Math.random()*datos.features.length)];
+       	placecoords=place.geometry.coordinates;
+	    placetag=place.properties.Name;
+	    showPics(placetag);
+	    map.on('click', onMapClick);
+	});
+}
+
+function showPics(placetag){
+    i=0;
+    $.getJSON(flickerAPI,{
+		tags:placetag,
+		tagmode:"any",
+		format:"json"
+	}).done(function(data){
+		data = data.items.splice(0,30);
+		myVar = setInterval(function () {myTimer()}, 1000/nivel);
+		function myTimer() {
+			document.getElementById("fotos").innerHTML = "";
+			html = '<img src="' + data[i].media.m + '"width="500px" height="280px">';
+			document.getElementById("fotos").innerHTML = html;
+			i++;
+			numPhotos++;
+			$("#photos").val(numPhotos);
+		}
+	});
+}
+
+function calcularPuntuacion(){
+    var puntuacion = dist*numPhotos;
+    $("#points").val(puntuacion.toFixed(3));
+}
+
+function onMapClick(e) {
+	var marker = "";
+	var lat = e.latlng.toString();
+	coords = lat.substring(6);
+	marker1 = L.marker(e.latlng).addTo(map);
+	$("#selectCoords").val(coords);
+	dist=e.latlng.distanceTo(L.latLng(placecoords[0], placecoords[1]))/1000;
+	$("#distance").val(dist.toFixed(3));
+	$("#correctCoords").val("("+placecoords+")");
+	$("#name").val(placetag);
+	marker2 = L.marker(L.latLng(placecoords[1], placecoords[0])).addTo(map);
+	clearTimeout(myVar);
+	calcularPuntuacion();
+	marcado = 1;
+	saveHistory();
+}
